da6c111c1cbc26c58e5ac67cc64fe7cb88628a5f
Granit <1>
2022-10-30 17:26:37 +0400

    форма-другое, прозрачная шапка+белый фон, плавный скролл

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,6 @@
-const INCREASE_NUMBER_ANIMATION_SPEED = 50;
 
+// Анимации увеличения числа клиентов
+const INCREASE_NUMBER_ANIMATION_SPEED = 50;
 function increaseNumberAnimationStep (i, element, endNumber) {
   if (i <= endNumber) {
     if (i === endNumber) {
@@ -18,8 +19,68 @@ function increaseNumberAnimationStep (i, element, endNumber) {
 
 function initIncreaseNumberAnimation() {
   const element = document.querySelector('.features__clients-count');
-  
   increaseNumberAnimationStep(0, element, 5000);
 }
 
-initIncreaseNumberAnimation();
+
+// Форма - другое
+document.querySelector("#budget").addEventListener("change", function handleSelectChange(event){
+  if (event.target.value === "other"){
+    const formContainer = document.createElement("div");
+    formContainer.classList.add("form__group", "form__other-input");
+
+    const input = document.createElement("input");
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text";
+
+    formContainer.appendChild(input)
+
+    document.querySelector("#form form").insertBefore(formContainer, document.querySelector(".form__submit"))
+  }
+  const otherInput = document.querySelector(".form__other-input")
+  if (event.target.value !== "other" && otherInput){
+    
+    document.querySelector("#form form").removeChild(otherInput)
+  }
+})
+
+
+// Изменяет header с прозрачного - на белый фон
+let animationInited = false;
+
+function updateScroll() {
+  if (window.scrollY > 0){
+    document.querySelector("header").classList.add("header__scrolled");
+  } else {
+    document.querySelector("header").classList.remove("header__scrolled");
+  }
+
+  // Запуск анимации увеличения числа
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+
+  if (windowBottomPosition >= countElementPosition && !animationInited){
+    animationInited = true;
+    initIncreaseNumberAnimation()
+  }
+}
+window.addEventListener("scroll", updateScroll);
+
+
+
+// Плавный скролл
+function addSmoothScroll(anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+ 
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  addSmoothScroll(anchor);
+});
+
+addSmoothScroll(document.querySelector('.more-button'));
